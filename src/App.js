65a5cc831111073ef220383f8da60330c5bb4f28
Chamith Lakshan <chamilucky8@gmail.com>
2022-11-17 02:04:56 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useWeb3React } from '@web3-react/core';
+import { InjectedConnector } from '@web3-react/injected-connector';
+import { getWeb3Provider } from '@web3-react/core';
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+ const { account, activate, deactivate, library } = useWeb3React()
+ const RPCCON = async () => new ethers.providers.JsonRpcProvider("https://data-seed-prebsc-1-s1.binance.org:8545");
+
+
+  const [walletstatus,setWalletstatus] = useState("Connect");
+
+
+  const walletHandler = async () => {
+    const selectedNetWorks = [56, 97, 137, 43114, 2, 3, 4, 42, 1]
+    const injCon = new InjectedConnector({supportedChainIds : selectedNetWorks})
+    await activate(injCon)
+//    console.log(injCon)
+  }
+  
+
+  return(
+  <div className="App">
+    <h1>Wallet Address : {account}</h1>
+    <button onClick={walletHandler}>{walletstatus}</button>
+     {console.log(walletstatus)}
+  </div>
   );
 }
 
